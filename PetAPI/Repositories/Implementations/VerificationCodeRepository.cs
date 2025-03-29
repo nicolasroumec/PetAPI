c9358f432631ba9ecd11e5d499608e987006e0d5
nicolasroumec <nicolasroumec@hotmail.com>
2025-03-29 12:23:39 -0300

    Add VerificationCodeRepository

---
+++ PetAPI/Repositories/Implementations/VerificationCodeRepository.cs
@@ -0,0 +1,30 @@
+﻿using PetAPI.Models;
+using PetAPI.Models.Entities;
+using PetAPI.Repositories.Interfaces;
+
+namespace PetAPI.Repositories.Implementations
+{
+    public class VerificationCodeRepository : BaseRepository<VerificationCode>, IVerificationCodeRepository
+    {
+        public VerificationCodeRepository(PetContext repositoryContext) : base(repositoryContext)
+        { 
+        }
+
+        public VerificationCode GetByUserIdAndCode(int userId, string code)
+        {
+            return FindByCondition(v => v.userId == userId && v.code == code && !v.isUsed)
+                .FirstOrDefault();
+        }
+
+        public void Save(VerificationCode code)
+        {
+            if (code.Id == 0)
+                Create(code);
+            else
+                Update(code);
+
+            SaveChanges();
+        }
+
+    }
+}
---
+++ PetAPI/Repositories/Interfaces/IVerificationCodeRepository.cs
@@ -0,0 +1,10 @@
+﻿using PetAPI.Models.Entities;
+
+namespace PetAPI.Repositories.Interfaces
+{
+    public interface IVerificationCodeRepository
+    {
+        VerificationCode GetByUserIdAndCode(int userId, string code);
+        void Save(VerificationCode code);
+    }
+}
