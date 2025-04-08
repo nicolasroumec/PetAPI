0384d1d800bc8ea7443a2475142e04744dfcee4e
nicolasroumec <nicolasroumec@hotmail.com>
2025-04-08 16:55:27 -0300

    Add PetsRepository

---
+++ PetAPI/Repositories/Implementations/PetsRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetAPI.Models;
+using PetAPI.Models.Entities;
+using PetAPI.Repositories.Interfaces;
+
+namespace PetAPI.Repositories.Implementations
+{
+    public class PetsRepository : BaseRepository<Pet>, IPetsRepository
+    {
+        public PetsRepository(PetContext repositoryContext) : base(repositoryContext) { }
+
+        public ICollection<Pet> GetPets(int shelterId)
+        {
+            return FindByCondition(p => p.ShelterId == shelterId)
+                .Include(p => p.Shelter)
+                .ToList();
+        }
+
+        public Pet GetPetById(int petId)
+        {
+            return FindByCondition(p => p.Id == petId)
+                .Include(p => p.Shelter)
+                .Include(p => p.adoptionRequests)
+                .FirstOrDefault();
+        }
+
+        public void UpdateState(Pet pet)
+        {
+            Update(pet);
+            SaveChanges();
+        }
+
+        public void Save(Pet pet)
+        {
+            if (pet.Id == 0)
+                Create(pet);
+            else
+                Update(pet);
+            SaveChanges();
+        }
+
+        public void Remove(Pet pet)
+        {
+            Delete(pet);
+            SaveChanges();
+        }
+    }
+}
---
+++ PetAPI/Repositories/Interfaces/IPetsRepository.cs
@@ -0,0 +1,13 @@
+﻿using PetAPI.Models.Entities;
+
+namespace PetAPI.Repositories.Interfaces
+{
+    public interface IPetsRepository
+    {
+        ICollection<Pet> GetPets(int shelterId);
+        Pet GetPetById(int petId);
+        void UpdateState(Pet pet);
+        void Save(Pet pet);
+        void Remove(Pet pet);
+    }
+}
